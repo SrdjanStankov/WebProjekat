752fa92276210adc0521ee5c31e0e827ea8ea88c
Srdjan <srki.miki@gmail.com>
2019-06-13 14:39:13 +0200

    added login form and login logic

--- ProjektniZadatak/Controllers/UserController.cs
+++ ProjektniZadatak/Controllers/UserController.cs
@@ -37,22 +37,41 @@ namespace ProjektniZadatak.Controllers
 
         public ActionResult Login(string username, string password)
         {
+            bool usernameFound = false;
+            bool passwordMatch = false;
+
+            if (username is null)
+            {
+                return View();
+            }
+
             foreach (var item in RegisteredUsers)
             {
                 if (item.Username == username)
                 {
+                    usernameFound = true;
                     if (item.Password == password)
                     {
                         item.IsLoggedIn = true;
+                        passwordMatch = true;
                         break;
-                    }
-                    else
-                    {
-                        ModelState.AddModelError("TEST", "ERROR");
                     }
                 }
             }
-            return View();
+
+            if (!usernameFound)
+            {
+                TempData["Username"] = "Username does not exist";
+                return View();
+            }
+
+            if(!passwordMatch)
+            {
+                TempData["Password"] = "Invalid password";
+                return View();
+            }
+
+            return View("LoogedIn");
         }
     }
 }
